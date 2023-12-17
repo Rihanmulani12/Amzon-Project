bab3bec356c789c0f61a3d8f17097fee3f667437
Rihan Mulani <rihanmulani18@gmail.com>
2023-12-17 18:21:22 +0530

    upadte cart qty active

--- amazon.js
+++ amazon.js
@@ -75,11 +75,20 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
       matchingItem.quantity += 1;
     } else {
       cart.push({
-        productId,
-        quantity: 1,
+        productId : productId,
+        quantity: 1
       });
     }
 
-    console.log(cart);
+    let cartQuantity = 0;
+
+    cart.forEach((item)=>{
+       cartQuantity+= item.quantity;
+    })
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
+
+    
   });
 });
