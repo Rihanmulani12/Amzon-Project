7639d5d0e9db311e17581787929cb274e8f5f785
Rihan Mulani <rihanmulani18@gmail.com>
2023-12-18 13:04:15 +0530

    Remove function add to cart

--- checkout.js
+++ checkout.js
@@ -1,4 +1,4 @@
-import { cart } from "./data/cart.js";
+import { cart  , removeFromCart } from "./data/cart.js";
 import { products } from "./data/products.js";
 
 let cartSumaaryHtml = '';
@@ -40,7 +40,8 @@ cart.forEach((CartItem) => {
             <span class="update-quantity-link link-primary">
               Update
             </span>
-            <span class="delete-quantity-link link-primary">
+            <span class="delete-quantity-link link-primary js-delete-link"
+             data-product-id="${matchingProduct.id}">
               Delete
             </span>
           </div>
@@ -94,6 +95,16 @@ cart.forEach((CartItem) => {
     </div>`
 
     document.querySelector('.js-order-summary').innerHTML = cartSumaaryHtml;
+
+    document.querySelectorAll('.js-delete-link').forEach((link)=>{
+     link.addEventListener('click' ,()=>{
+        const productId = link.dataset.productId;
+        removeFromCart(productId);
+        
+     })
+    })
+
+   
  
    
 })
