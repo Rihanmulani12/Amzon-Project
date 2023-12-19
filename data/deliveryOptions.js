16cad89f350658b962fdac6c4a4c6d71b92b69e4
Rihan Mulani <rihanmulani18@gmail.com>
2023-12-19 11:13:09 +0530

    add deliveryOptiops for real time date and time

---
+++ data/deliveryOptions.js
@@ -0,0 +1,12 @@
+ export const deliveryOptions = [
+    {
+        id : "1",
+        deliveryDay : 2,
+        price : 0
+    },
+    {
+        id : "2",
+        deliveryDay : 2,
+        price : 50
+    }
+]
