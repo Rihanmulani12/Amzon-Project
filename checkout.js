3cf2de6b0230bf6643fcb113a02d0303699b6be4
Rihan Mulani <rihanmulani18@gmail.com>
2023-12-20 13:24:49 +0530

    finally done order summary and payment summary

--- checkout.js
+++ checkout.js
@@ -1,2 +1,5 @@
-import { renderOrderSummary } from "./orderSummary.js";
+import {renderOrderSummary} from './checkout/orderSummary.js';
+import {renderPaymentSummary} from './checkout/paymentSummary.js';
+
 renderOrderSummary();
+renderPaymentSummary();
--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -1,12 +1,25 @@
- export const deliveryOptions = [
-    {
-        id : "1",
-        deliveryDay : 5,
-        price : 0
-    },
-    {
-        id : "2",
-        deliveryDay : 3,
-        price : 50
-    }
-]
+export const deliveryOptions = [{
+    id: '1',
+    deliveryDays: 7,
+    price: 0
+  }, {
+    id: '2',
+    deliveryDays: 3,
+    price: 499
+  }, {
+    id: '3',
+    deliveryDays: 1,
+    price: 999
+  }];
+  
+  export function getDeliveryOption(deliveryOptionId) {
+    let deliveryOption;
+  
+    deliveryOptions.forEach((option) => {
+      if (option.id === deliveryOptionId) {
+        deliveryOption = option;
+      }
+    });
+  
+    return deliveryOption;
+}
